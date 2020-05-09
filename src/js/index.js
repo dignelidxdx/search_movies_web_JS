d2ea2cf031fca8ac4ba157e0a84408819170c273
dignelidxdx <digneli14@gmail.com>
2020-05-08 23:07:53 -0300

    Recharging CSS and adding a new API for friends

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
+/* ------------------ */
+/* VARIABLES GLOBALES */
+/* ------------------ */
+
+// External Service
+
+const MOVIE_API = 'https://yts.mx/api/v2/';
+const USER_API = 'https://randomuser.me/api/?inc=name,picture&results=';
+
+// Conteiners
+
+const $form = document.getElementById('form');
+const $home = document.getElementById('home');
+const $featuringContainer = document.getElementById('featuring');
+const $loader = document.createElement('img');
+const $actionContainer = document.querySelector('#action');
+const $dramaContainer = document.querySelector('#drama');
+const $animationContainer = document.querySelector('#animation');
+const $modal = document.getElementById('modal');
+const $overlay = document.getElementById('overlay');
+const $hideModal = document.getElementById('hide-modal');
+const $modalTitle = $modal.querySelector('h1');
+const $modalImage = $modal.querySelector('img');
+const $modalDescription = $modal.querySelector('p');
+const $userName = document.getElementById('playlistFriends');
+
+
+/* ------------------------------------------------------------------------- */
+/* TRABAJAMOS CON EL FORMULARIO DE BUSQUEDA, EL API Y LA SECCION DE BUSQUEDA */
+/* ------------------------------------------------------------------------- */
+
+
+/* --------------------------------------------------------------------------------------- */
+/* TRABAJAMOS CON PETICIONES AL API Y CON LOS RESULTADOS CREAMOS DIFERENTES ELEMENTOS HTML */
+/* --------------------------------------------------------------------------------------- */
+
 (async function load() {
 
 
@@ -15,18 +51,14 @@
         const response = await fetch(url);
         const data = await response.json();
         return data;
-    }
-  
-    const $form = document.getElementById('form');
-    const $home = document.getElementById('home');
-    const $featuringContainer = document.getElementById('featuring');
+    }   
   
     function setAttributes($element, attributes) {
       for (const attribute in attributes) {
         $element.setAttribute(attribute, attributes[attribute]); 
       }
     }
-    const BASE_API = 'https://yts.mx/api/v2/';
+    
   
     function featuringTemplate(peli) {
       return (
@@ -56,15 +88,30 @@
         </div> 
         `       
     )}
+
+    function userTemplate(user) {
+      return (
+        `
+        <li class="playlistFriends-item">
+          <a href="#">
+            <img src="${user.picture.medium} " alt="echame la culpa" />
+            <span>
+              ${user.name.first} ${user.name.last}
+            </span>
+          </a>
+        </li>
+        `
+      )
+    }
     
     $form.addEventListener('submit', async (event) => {
       event.preventDefault();
       $home.classList.add('search-active');
-      const $loader = document.createElement('img');
+      
       setAttributes($loader, {
         src: 'src/images/loader.gif',
         height: 50,
-        width: 50,
+        width: 50,      
       })
       $featuringContainer.append($loader);
   
@@ -74,7 +121,7 @@
           data: {
             movies: pelis
           }
-        } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`);
+        } = await getData(`${MOVIE_API}list_movies.json?limit=1&query_term=${data.get('name')}` );
         const HTMLString = featuringTemplate(pelis[0]);
         $featuringContainer.innerHTML = HTMLString;
       } catch(error) {
@@ -83,9 +130,7 @@
         $home.classList.remove('search-active');
       }
       
-    })
-  
-    
+    })    
     
     function createTemplate(HTMLString) {
       const html = document.implementation.createHTMLDocument();
@@ -115,6 +160,18 @@
         addEventClick(movieElement);
       } )
     }
+
+    function renderUser(list, $container) {
+      list.forEach((result) => {
+        const HTMLString = userTemplate(result);
+        const userElement = createTemplate(HTMLString);
+        $container.append(userElement);
+      } )
+    }
+
+    const { results: friends } = await getDataUser(`${USER_API}5`);
+
+    renderUser(friends, $userName);
   
     async function cacheExist(category) {
       const listName = `${category}List`;
@@ -123,7 +180,7 @@
       if (cacheList) {
         return JSON.parse(cacheList);
       }
-      const { data: { movies: data } } = await getData(`${BASE_API}list_movies.json?genre=${category}`);
+      const { data: { movies: data } } = await getData(`${MOVIE_API}list_movies.json?genre=${category}`);
       window.localStorage.setItem(listName, JSON.stringify(data))
   
       return data;
@@ -131,37 +188,26 @@
     }
     
   
-    // const { data: { movies: actionList} } = await getData(`${BASE_API}list_movies.json?genre=action`)
+    // const { data: { movies: actionList} } = await getData(`${MOVIE_API}list_movies.json?genre=action`)
     // window.localStorage.setItem('actionList', JSON.stringify(actionList));
     const actionList = await cacheExist('action');
-    const $actionContainer = document.querySelector('#action');
+    
     // renderMovieList(actionList.data.movies, $actionContainer, action);
     renderMovieList(actionList, $actionContainer, "action");
   
-    // const { data: { movies: dramaList } } = await getData(`${BASE_API}list_movies.json?genre=drama`)
+    // const { data: { movies: dramaList } } = await getData(`${MOVIE_API}list_movies.json?genre=drama`)
     // window.localStorage.setItem('dramaList', JSON.stringify(dramaList));
     const dramaList = await cacheExist('drama');
-    const $dramaContainer = document.querySelector('#drama');
+    
     // renderMovieList(dramaList.data.movies, $dramaContainer, drama);
     renderMovieList(dramaList, $dramaContainer, "drama");
     
-    const { data: { movies: animationList } } = await getData(`${BASE_API}list_movies.json?genre=animation`)
+    const { data: { movies: animationList } } = await getData(`${MOVIE_API}list_movies.json?genre=animation`)
     window.localStorage.setItem('animationList', JSON.stringify(animationList));
     // const animationList = await cacheExist('animaton');
-    const $animationContainer = document.querySelector('#animation');
     // renderMovieList(animationList.data.movies, $animationContainer, animation);
     renderMovieList(animationList, $animationContainer, "animation");
-     
-    
-    const $modal = document.getElementById('modal');
-    const $overlay = document.getElementById('overlay');
-    const $hideModal = document.getElementById('hide-modal');
-  
-    // document.querySelector('#modal img')
-    const $modalTitle = $modal.querySelector('h1');
-    const $modalImage = $modal.querySelector('img');
-    const $modalDescription = $modal.querySelector('p');
-  
+       
     function findById(list, id) {
       return list.find(movie => movie.id === parseInt(id, 10))
     }
@@ -179,8 +225,7 @@
         default: {
           findById(actionList, id)
         }
-      }
-      
+      }      
     }
   
     function showModal($element) {
@@ -199,9 +244,6 @@
       $overlay.classList.remove('active');
       $modal.style.animation = 'modalOut .8s forwards';
     }
-  
-  
-  
-  
+    
   
   })()
